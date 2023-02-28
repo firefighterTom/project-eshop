eb870c58328ecf9dc055a2a55bcc439841873506
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-02-28 21:30:48 +0100

    created new folder utilities and moved function getEnv to this folder

--- apollo/client.tsx
+++ apollo/client.tsx
@@ -1,12 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { getEnv } from 'utilities/getEnv';
 
-function getEnv<T>(env: T): T {
-	if (env) return env;
-
-	throw new Error('Fail env');
-}
 
 export const client = new ApolloClient({
-	uri: getEnv(process.env.NEXT_PUBLIC_HYGRAPH_API),
+	uri: getEnv(process.env?.NEXT_PUBLIC_HYGRAPH_API),
 	cache: new InMemoryCache(),
 });
