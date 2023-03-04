b50446af51a3143c97ee15004b862105f066aea1
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-03-04 12:37:30 +0100

    fixed error

--- apollo/client.tsx
+++ apollo/client.tsx
@@ -3,6 +3,6 @@ import { getEnv } from 'utilities/getEnv';
 
 
 export const client = new ApolloClient({
-	uri: getEnv(process.env?.NEXT_PUBLIC_HYGRAPH_API),
+	uri: getEnv(process.env.NEXT_PUBLIC_HYGRAPH_API),
 	cache: new InMemoryCache(),
 });
