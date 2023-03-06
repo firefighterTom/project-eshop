71543009c1b6de41dd4a44d8fe4e016554871f3d
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-03-06 23:21:52 +0100

    created cart context for user

--- pages/products/[products_name].tsx
+++ pages/products/[products_name].tsx
@@ -1,17 +1,32 @@
+import { useCartContext } from 'context/contextCart';
 import { useGetProductBySlugQuery } from 'generated/graphql';
 import { useRouter } from 'next/router';
 
+
 export default function ProductPage() {
+	const { addToCart } = useCartContext();
 	const router = useRouter();
 	const productName = Array.isArray(router.query.products_name)
 		? router.query.products_name[0]
 		: router.query.products_name;
+
 	if (productName) {
 		const { data } = useGetProductBySlugQuery({
 			variables: { slug: productName },
 		});
 		if (!data) return <h2>Problem with fetching</h2>;
 
-		return <pre>{JSON.stringify(data, null, 2)}</pre>;
+		return (
+			<>
+				<pre>{JSON.stringify(data, null, 2)}</pre>
+				<button
+					className='bg-black text-white py-2 px-3 '
+					onClick={() => {
+						if (data.product) addToCart(data.product.name);
+					}}>
+					Add
+				</button>
+			</>
+		);
 	}
 }
