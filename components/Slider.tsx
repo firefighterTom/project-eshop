d7c0a8db9236d4111e26a4afea33fe7ceedeb8b9
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-05-22 22:19:30 +0200

    added slider

---
+++ components/Slider.tsx
@@ -0,0 +1,38 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { GetProductsQuery } from 'generated/graphql';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Autoplay, Navigation } from 'swiper';
+type ProductsProps = {
+	products: GetProductsQuery;
+};
+export function Slider(props: ProductsProps) {
+	console.log(props.products.products);
+	return (
+		<>
+			<h2 className='text-center mb-10'>Bestseller</h2>
+			<Swiper
+				slidesPerView={3}
+				spaceBetween={30}
+				loop={true}
+				autoplay={{
+					delay: 5000,
+					disableOnInteraction: false,
+				}}
+				navigation={true}
+				modules={[Autoplay, Navigation]}
+				className='text-center '>
+				{props.products.products.map((product) => {
+					const productImg = product.images[0].url;
+					const productName = product.name;
+					return (
+						<SwiperSlide key={product.id}>
+							<img className='h-28 m-auto' src={productImg} alt={productName} />
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
+import { Slider } from 'components/Slider';
 import { useGetProductsQuery } from 'generated/graphql';
 import Link from 'next/link';
 
 export default function Home() {
 	const { data, loading, error } = useGetProductsQuery();
 	if (!data) return <h2>Problem with fetching</h2>;
-	return data.products.map(({ slug, name, id }) => (
-		<Link key={id} href={`products/${slug}`}>
-			<h2>{name}</h2>
-		</Link>
-	));
+	return (
+		<>
+			{data.products.map(({ slug, name, id }) => (
+				<Link key={id} href={`products/${slug}`}>
+					<h2>{name}</h2>
+				</Link>
+			))}
+			<Slider products={data}></Slider>
+		</>
+	);
 }
