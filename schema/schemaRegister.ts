c46df0a8c5f0931193c0d8ba3b793969538ae2cb
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-09-07 16:46:25 +0200

    schema of login and registration has been separated

--- schema/schemaRegister.ts
+++ schema/schemaRegister.ts
@@ -3,6 +3,10 @@ import * as yup from "yup";
 export const loginSchema = yup.object({
     email: yup.string().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid mail').required('Mail is required'),
     password: yup.string().required('Password is a required').min(4).max(10),
+  });
+  export const registrationSchema = yup.object({
+    email: yup.string().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid mail').required('Mail is required'),
+    password: yup.string().required('Password is a required').min(4).max(10),
     passwordConfirmation: yup.string()
     .oneOf([yup.ref('password')], 'Passwords must match').required('Password confirmation is required')
   });
