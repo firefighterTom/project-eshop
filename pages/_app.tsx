08a744445bf7f501aaf352ec40cae03b853be776
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-09-07 16:53:39 +0200

    added SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
-import { admin, client } from 'apollo/client';
+import { client } from 'apollo/client';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react"
 import '../styles/global.css'
 import { Layout } from 'components/Layout';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
+		<SessionProvider session={session}>
 		<ApolloProvider client={client}>
 			<Layout>
 			<Component {...pageProps} />
 			</Layout>
 		</ApolloProvider>
+		</SessionProvider>
 	);
 }
