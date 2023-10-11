cfef29f58463ef05c976b391f1dac5b9c2a98241
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-10-11 18:42:47 +0200

    changed max-width

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,11 +10,11 @@ import { Nav } from './Nav';
 export function Layout({ children }: PropsWithChildren) {
 	const addNotificationContext = useAddNotificationContext();
 	return (
-		<div className='max-w-7xl mx-auto'>
+		<div className='max-w-screen-2xl mx-auto'>
 			<AddNotificationProvider>
 				<CartProvider>
 					<header>
-						<Nav />
+						<Nav/>
 					</header>
 					<main>{children}</main>
 					<footer></footer>
