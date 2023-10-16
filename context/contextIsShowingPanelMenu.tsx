fd6d4fd544224d107fe91caead8c667cbc614ed4
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-10-16 20:53:50 +0200

    created context to showing panel menu

---
+++ context/contextIsShowingPanelMenu.tsx
@@ -0,0 +1,35 @@
+import { type } from 'os';
+import { PropsWithChildren, createContext, useContext, useState } from 'react';
+
+type ShowingPanelContextType = {
+	isOpen: boolean;
+	openPanelMenu: () => void;
+	closePanelMenu: () => void;
+};
+const ShowingPanelMenuContext = createContext<
+	ShowingPanelContextType | undefined
+>(undefined);
+
+export function ShowingPanelMenuProvider({ children }: PropsWithChildren) {
+	const [isOpen, setIsOpen] = useState(false);
+	const openPanelMenu = () => {
+		setIsOpen(!isOpen);
+	};
+	const closePanelMenu = () => {
+		setIsOpen(!isOpen);
+	};
+	return (
+		<ShowingPanelMenuContext.Provider
+			value={{ isOpen, openPanelMenu, closePanelMenu }}>
+			{children}
+		</ShowingPanelMenuContext.Provider>
+	);
+}
+
+export const useShowingPanelMenuContext = () => {
+	const showingPanelMenuContext = useContext(ShowingPanelMenuContext);
+	if (showingPanelMenuContext === undefined) {
+		throw new Error('ShowingPanelMenuContext is undefined');
+	}
+	return showingPanelMenuContext;
+};
