1257584bf1ee9f16ac32a76067a39b2b47fe06cc
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-10-22 21:57:59 +0200

    changed style

--- components/Nav/PanelMenu.tsx
+++ components/Nav/PanelMenu.tsx
@@ -11,23 +11,28 @@ export function PanelMenu() {
 	];
 	return (
 		<>
-			<div className='absolute bg-slate-300 w-full h-screen left-0 top-0 z-40 bg-opacity-75 blur-xl '></div>
-			<div className='absolute bg-slate-400 text-black left-0 top-0 h-screen z-40 flex rounded-r-2xl '>
-				<div className='relative  text-center uppercase mt-8 '>
-					<Link href={'/'} className='block font-bold'>
+			<div className='absolute bg-slate-300 w-full h-screen left-0 top-0 z-20 bg-opacity-75 blur-xl '></div>
+			<div className='absolute bg-[#534b52] text-white left-0 top-0 h-screen z-40 flex w-1/3 min-w-[13rem] max-w-[15rem]  '>
+				<div className='relative mt-8 w-full'>
+					<Link
+						onClick={closePanelMenu}
+						href={'/'}
+						className='block font-bold uppercase text-2xl pl-2 inline-block '>
 						E-shop
 					</Link>
-					<button onClick={closePanelMenu} className='absolute right-2 -top-2'>
+					<button
+						onClick={closePanelMenu}
+						className='absolute right-3 top-[1px]'>
 						<IconCloseMenuBar />
 					</button>
-					<div className='mt-7'>
+					<div className='mt-9  '>
 						{linkNav.map((el) => {
 							return (
 								<Link
 									key={el.linkName}
 									onClick={closePanelMenu}
 									href={`${el.link}`}
-									className='block mb-4 hover:bg-white duration-200 py-2  px-16 '>
+									className='block py-3 hover:bg-white hover:text-[#534b52] duration-200 py-2  pl-2  border solid border-0 border-b-[1px]  '>
 									{el.linkName}
 								</Link>
 							);
