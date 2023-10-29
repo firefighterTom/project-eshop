1d51d6b871bf3e0b2e9f043f9735ad35f982f314
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-10-29 18:03:22 +0100

    The Star component has been removed, and the Star Icon has been imported and added

--- components/Reviews/Stars.tsx
+++ components/Reviews/Stars.tsx
@@ -1,28 +1,4 @@
-type Star = {
-	starColor: string;
-};
-
-function Star(props: Star) {
-	return (
-		<svg
-			xmlns='http://www.w3.org/2000/svg'
-			className='icon icon-tabler icon-tabler-star-filled'
-			width={18}
-			height={24}
-			viewBox='0 0 24 24'
-			strokeWidth='1.25'
-			stroke='currentColor'
-			fill='none'
-			strokeLinecap='round'
-			strokeLinejoin='round'>
-			<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-			<path
-				d='M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z'
-				strokeWidth={0}
-				fill={props.starColor}></path>
-		</svg>
-	);
-}
+import Star from './../../assets/star.svg';
 
 type RatingProp = {
 	rating?: number | null;
@@ -35,13 +11,16 @@ export function Stars(props: RatingProp) {
 					return (
 						<li>
 							{props.rating > el ? (
-								<Star key={el} starColor='#d4af37' />
+								<Star
+									key={el}
+									className='fill-gold stroke-gold w-5 h-5'
+								/>
 							) : (
-								<Star key={el} starColor='#333' />
+								<Star key={el} className='stroke-black w-5 h-5' />
 							)}
 						</li>
 					);
-				return <Star key={el} starColor='#333' />;
+				return <Star key={el} className=' stroke-black w-5 h-5' />;
 			})}
 		</ul>
 	);
