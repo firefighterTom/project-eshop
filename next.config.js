e0867496c5885fcc32763e5e600a5efc36d8eac0
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-10-29 18:06:57 +0100

    added domains

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		domains: ['media.graphassets.com'],
+	},
 	reactStrictMode: true,
 	webpack(config) {
 		config.module.rules.push({
-		  test: /\.svg$/i,
-		  issuer: /\.[jt]sx?$/,
-		  use: ["@svgr/webpack"],
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
 		});
-	
+
 		return config;
-	  },
+	},
 };
 
 module.exports = nextConfig;
