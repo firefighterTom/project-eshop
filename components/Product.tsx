97e6e601e045729caebeeeb56edddc3d38f3a91d
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-10-29 17:55:16 +0100

    Implemented handleMouseEnter, handleMouseLeave averageReviewScore functions and added ProductHover component

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,28 +1,58 @@
+import { averageReviewScore } from 'utilities/avarageReviewScore';
 import { Stars } from './Reviews/Stars';
+import Image from 'next/image';
+import { useState } from 'react';
+import { ProductHover } from './ProductHover';
 
 type ProductProps = {
 	name: string;
 	img: string;
+	id: string;
 	price: number;
 	reviews: { rating?: number | null }[];
 	linkToProduct: string;
 };
 
 export function Product(props: ProductProps) {
-	console.log(props.img);
-	const avarageReviewsScore = props.reviews.length
-		? props.reviews.reduce((sum, currentValue) => {
-				if (currentValue.rating === null || !currentValue.rating) return sum;
-				return currentValue.rating + sum;
-		  }, 0) / props.reviews.length
-		: 0;
+	const [isHovered, setIsHovered] = useState(false);
 
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+	const avarageReviewsScore = props.reviews.length
+		? averageReviewScore(props.reviews)
+		: 0;
 	return (
-		<div className='flex flex-col items-center'>
-			<img className='h-28 w-28 ' src={props.img} alt={props.name} />
-			<p className='font-bold'>{props.name}</p>
-			<p className=''>{props.price} $</p>
+		<div
+			className='flex flex-col items-center relative group  '
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}>
+			<Image
+				className='h-28 w-28 '
+				width={112}
+				height={112}
+				src={props.img}
+				alt={props.name}
+			/>
+			<h3 className='font-bold'>{props.name}</h3>
+			<p>
+				{Intl.NumberFormat('pl-PL', {
+					style: 'currency',
+					currency: 'PLN',
+				}).format(props.price)}
+			</p>
 			<Stars rating={avarageReviewsScore} />
+			{isHovered && (
+				<ProductHover
+					name={props.name}
+					id={props.id}
+					linkToProduct={props.linkToProduct}
+				/>
+			)}
 		</div>
 	);
 }
