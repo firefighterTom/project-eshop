f305d713a0bed91df7897466d5c31d9b8ec17d95
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-02 11:22:36 +0100

    deleted useEffect and useState and added fill to Image

--- components/Header/SliderHeader.tsx
+++ components/Header/SliderHeader.tsx
@@ -1,39 +1,17 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import BiggerImgShoes from '../../images/shoesBiggerScreen.jpg';
-import SmallerImgShoes from '../../images/shoesSmallScreen.jpg';
-import BiggerImgBikes from '../../images/bikesBiggerScreen.jpg';
-import SmallerImgBikes from '../../images/bikesSmallerScreen.jpg';
-import BiggerImgTennis from '../../images/tennisBiggerScreen.jpg';
-import SmallerImgTennis from '../../images/tennisSmallerScreen.jpg';
+import ShoesImg from '../../images/shoesImg.jpg';
+import BikeImg from '../../images/bikeImg.jpg';
+import TennisImg from '../../images/tennisImg.jpg';
 import { Autoplay } from 'swiper';
-import Image, { StaticImageData } from 'next/image';
-import { useEffect, useState } from 'react';
-
-type ImgKeeper = {
-	src: StaticImageData;
-	name: string;
-	alt: string;
-};
+import Image from 'next/image';
 
 export default function SliderHeader() {
-	const imgForBiggerScreen = [
-		{ src: BiggerImgShoes, name: 'BiggerImgShoes', alt: 'Basketball shoes' },
-		{ src: BiggerImgBikes, name: 'BiggerImgBikes', alt: 'Stationary bikes' },
-		{ src: BiggerImgTennis, name: 'BiggerImgTennis', alt: 'Tennis racket' },
+	const imgages = [
+		{ src: ShoesImg, name: 'ShoesImg', alt: 'Basketball shoes' },
+		{ src: BikeImg, name: 'BikeImg', alt: 'Stationary bikes' },
+		{ src: TennisImg, name: 'TennisImg', alt: 'Tennis racket' },
 	];
-	const imgForSmallerScreen = [
-		{ src: SmallerImgShoes, name: 'SmallerImgShoes', alt: 'Basketball shoes' },
-		{ src: SmallerImgBikes, name: 'SmallerImgBikes', alt: 'Stationary bikes' },
-		{ src: SmallerImgTennis, name: 'SmallerImgTennis', alt: 'Tennis racket' },
-	];
-	const [imgSrc, setImgSrc] = useState<ImgKeeper[]>([]);
-	useEffect(() => {
-		if (window.innerWidth < 800) {
-			setImgSrc(imgForSmallerScreen);
-		} else {
-			setImgSrc(imgForBiggerScreen);
-		}
-	}, [window.innerWidth]);
+
 	return (
 		<>
 			<Swiper
@@ -44,17 +22,18 @@ export default function SliderHeader() {
 				loop={true}
 				modules={[Autoplay]}
 				className='mySwiper'>
-				{imgSrc.map((photo) => {
+				{imgages.map((photo) => {
 					return (
 						<SwiperSlide key={photo.name}>
-							<Image
-								priority={true}
-								src={photo.src}
-								width={0}
-								height={0}
-								className='w-full h-[70vh] '
-								alt={photo.alt}
-							/>
+							<div className=' relative h-[60vh] lg:h-[70vh] '>
+								<Image
+									priority={true}
+									src={photo.src}
+									fill
+									className='object-cover '
+									alt={photo.alt}
+								/>
+							</div>
 						</SwiperSlide>
 					);
 				})}
