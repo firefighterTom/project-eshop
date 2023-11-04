482b243312e878a07790863783a42da08b12eb97
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-04 11:59:14 +0100

    changed name of the file

---
+++ context/cart.tsx
@@ -0,0 +1,59 @@
+import { PropsWithChildren, useContext, useEffect, useState } from 'react';
+import { createContext } from 'react';
+
+import { useAddNotificationContext } from './addNotification';
+
+import { addProductToCart, validationLocalStorage } from './utilsCartContext';
+type itemsCartType = {
+	name: string;
+	id: string;
+	amount: number;
+}[];
+type CartContextType = {
+	items: itemsCartType;
+	addToCart: (value: addedProduct) => void;
+};
+
+type addedProduct = {
+	name: string;
+	id: string;
+};
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+export function CartProvider({ children }: PropsWithChildren) {
+	const addNotificationContext = useAddNotificationContext();
+
+	const [items, setItems] = useState<itemsCartType>([]);
+	useEffect(() => {
+		try {
+			validationLocalStorage(setItems);
+		} catch (error) {
+			setItems([]);
+		}
+	}, []);
+	useEffect(() => {
+		if (items.length)
+			window.localStorage.setItem('cart', JSON.stringify(items));
+	}, [items]);
+
+	const addToCart = (element: addedProduct) => {
+		addNotificationContext?.showNotification();
+		addNotificationContext?.closeOnTimeNotification();
+		const chosenProduct = addProductToCart(element, items);
+		if (chosenProduct) setItems(chosenProduct);
+	};
+
+	return (
+		<CartContext.Provider value={{ items, addToCart }}>
+			{children}
+		</CartContext.Provider>
+	);
+}
+
+export const useCartContext = () => {
+	const cartContext = useContext(CartContext);
+	if (cartContext === undefined) {
+		throw new Error('CartContext is undefined');
+	}
+	return cartContext;
+};
