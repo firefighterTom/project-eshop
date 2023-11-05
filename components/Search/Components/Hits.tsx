c34ed86e6afcb6b758baaaa9d6f946275740dc3d
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-05 15:24:40 +0100

    Added display of product images, display of prices in PLN currency format

--- components/Search/Components/Hits.tsx
+++ components/Search/Components/Hits.tsx
@@ -1,17 +1,38 @@
 import { StateResultsProvided } from 'react-instantsearch-core';
 import { connectStateResults } from 'react-instantsearch-dom';
+import Image from 'next/image';
 
 export function Hits({
 	allSearchResults,
 	searchState,
-}: StateResultsProvided<{ name: string; description: string }>) {
+}: StateResultsProvided<{
+	name: string;
+	description: string;
+	price: number;
+	images: { url: string }[];
+	reviews: [];
+}>) {
 	if (!searchState?.query) return <></>;
 	return (
 		<ul>
 			{allSearchResults.hits.map((product) => (
 				<li key={product.objectID}>
-					<article>
-						<h3>{product.name}</h3>
+					<article className='grid grid-rows-2 grid-cols-[1fr_2fr_1fr] text-sm'>
+						<Image
+							width={50}
+							height={50}
+							src={product.images[0].url}
+							alt={product.name}
+							className=' sm:w-[80px] sm:h-[80px] row-span-full justify-self-center'
+						/>
+						<h3 className=''>{product.name}</h3>
+						<p className=' row-span-full col-start-3 self-center'>
+							{Intl.NumberFormat('pl-PL', {
+								style: 'currency',
+								currency: 'PLN',
+							}).format(product.price)}
+						</p>
+						<p>rating</p>
 					</article>
 				</li>
 			))}
