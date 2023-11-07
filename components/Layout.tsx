62594fd22eca4b6069a24f11e3cc6ec35c1aed74
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-08 00:18:24 +0100

    added to main style -grow and for parent flex flex-col and h-full

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,12 +7,12 @@ import { Notification } from './Notification/Notification';
 export function Layout({ children }: PropsWithChildren) {
 	const context = useShowingComponentContext();
 	return (
-		<div className='max-w-screen-2xl mx-auto'>
+		<div className='max-w-screen-2xl mx-auto flex flex-col h-full'>
 			{context.addedToCartNotificationComponent.isOpen && <Notification/>}
 					<header className='sticky top-0 z-20 '>
 						<Nav />
 					</header>
-					<main>{children}</main>
+					<main className='grow'>{children}</main>
 					<footer></footer>
 				
 		</div>
