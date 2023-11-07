5bcc241e134eb5cd1924c6212e2f42dbf58dde36
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-07 23:36:27 +0100

    improved a little bit style but it will be more

--- components/Reviews/Reviews.tsx
+++ components/Reviews/Reviews.tsx
@@ -1,18 +1,17 @@
-import { Stars } from "./Stars";
+import { Stars } from './Stars';
 
 type ReviewsProps = {
 	name: string;
 	content: string;
-	rating?:number|null
+	rating?: number | null;
 };
 
 export function Reviews(props: ReviewsProps) {
 	return (
-		<div className='w-40 border border-black text-center'>
-			<p>Review</p>
-			<p>{props.name}</p>
-			<p>{props.content}</p>
-			<Stars rating={props.rating}/>
+		<div className=' p-2 w-[15rem] m-auto shadow-[5px_5px_5px_rgba(0,0,0,0.3)] text-center '>
+			<p className='font-bold'>{props.name}</p>
+			<p className=''>{props.content}</p>
+			<Stars rating={props.rating} />
 		</div>
 	);
 }
