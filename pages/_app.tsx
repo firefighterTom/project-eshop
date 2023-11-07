0b7615f29605f9a69ad4a48abf052fb5c9ea88cb
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-07 23:40:56 +0100

    added ShowingComponentProvider and CartProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,20 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react"
 import '../styles/global.css'
 import { Layout } from 'components/Layout';
+import { ShowingComponentProvider } from 'context/showingComponent';
+import { CartProvider } from 'context/cart';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
 		<SessionProvider session={session}>
 		<ApolloProvider client={client}>
+		<ShowingComponentProvider>
+				<CartProvider>
 			<Layout>
 			<Component {...pageProps} />
 			</Layout>
+			</CartProvider>
+			</ShowingComponentProvider>
 		</ApolloProvider>
 		</SessionProvider>
 	);
