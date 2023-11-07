fd1be5f9cd6fe035b0b4bc00a45e10091e7bf668
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-07 23:12:56 +0100

    added breakpoint xs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    fontFamily:{
-      'gabarito':['"Gabarito"', 'sans-serif']
-    },
-    screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		fontFamily: {
+			gabarito: ['"Gabarito"', 'sans-serif'],
+		},
+		screens: {
+			xs: '400px',
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
-  },
-  plugins: [],
-}
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
+	},
+	plugins: [],
+};
