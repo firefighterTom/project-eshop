52e5e86803a1f22810902ad619b6e62df2567126
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-10 15:20:14 +0100

    The path of the image in the src changed.

--- pages/Account.tsx
+++ pages/Account.tsx
@@ -1,14 +1,13 @@
 import { Login } from 'components/LoginPage/Login';
 import { Registration } from 'components/LoginPage/RegistrationPage';
 import Image from 'next/image'
-import backgroundImage from '../images/sports-4901000_1920.jpg'
 import {  useState } from 'react';
 
 export default function LoginPage() {
 	const [isOpen,setIsOpen]=useState(false)
 	return (
 		<div className=' h-full  relative flex flex-col justify-center items-center '>
-			<Image src={backgroundImage} fill  className=' -z-10 object-cover ' alt='Ball in the corner'/>
+			<Image src='/tennisBallAndShoes.jpg' fill  className=' -z-10 object-cover ' alt='Ball in the corner'/>
 			{isOpen && <Login  isOpen={isOpen} setIsOpen={setIsOpen}/>}
 			{!isOpen &&<Registration isOpen={isOpen} setIsOpen={setIsOpen}/>}
 			</div>
