3bd235e27c2984eb3c7a34d5acb047447d219526
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-12 18:34:14 +0100

    added validation name

--- schema/schemaRegister.ts
+++ schema/schemaRegister.ts
@@ -5,6 +5,7 @@ export const loginSchema = yup.object({
     password: yup.string().required('Password is a required').min(4).max(10),
   });
   export const registrationSchema = yup.object({
+    name: yup.string().required('This field is a required').min(3).max(20),
     email: yup.string().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid mail').required('Mail is required'),
     password: yup.string().required('Password is a required').min(4).max(10),
     passwordConfirmation: yup.string()
