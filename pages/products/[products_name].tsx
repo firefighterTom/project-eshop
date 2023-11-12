6380785f380e4ba167c1ebb11cd51687f154d0f1
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-12 18:33:37 +0100

    added price and img to product

--- pages/products/[products_name].tsx
+++ pages/products/[products_name].tsx
@@ -3,16 +3,15 @@ import { useGetProductBySlugQuery } from 'generated/graphql';
 import { useRouter } from 'next/router';
 import IconMinus from '../../assets/icon-minus.svg';
 import IconPlus from '../../assets/icon-plus.svg';
-import { SelectAndDisplay } from 'components/SelectAndDisplay';
-import { Photos } from 'components/ProductPhotos';
+import { SelectAndDisplay } from 'components/SelectAndDisplay/SelectAndDisplay';
+import { Photos } from 'components/Product/components/ProductImages';
 import { useState } from 'react';
-import { Stars } from 'components/Reviews/Stars';
+import { Stars } from 'components/Stars/Stars';
 import { averageReviewScore } from 'utilities/avarageReviewScore';
 
 export default function ProductPage() {
 	const { addToCart } = useCartContext();
 	const [amountOfProducts, setAmountOfProducts] = useState(1);
-
 	const router = useRouter();
 	const productName = Array.isArray(router.query.products_name)
 		? router.query.products_name[0]
@@ -25,38 +24,37 @@ export default function ProductPage() {
 	const avarageReviewsScore = data.reviews.length
 		? averageReviewScore(data.reviews)
 		: 0;
-		console.log(avarageReviewsScore)
 	return (
 		<div className='flex flex-col items-center mt-5'>
 			<div className='max-w-[1000px] '>
-				<div className=' w-full grid grid-cols-3 sm:grid-cols-2  '>
+				<div className=' grid grid-cols-3 w-full sm:grid-cols-2  '>
 					<Photos name={data.product.name} images={data.product.images} />
-					<h2 className='col-span-full justify-self-center sm:row-start-1 sm:col-start-2 sm:justify-self-start uppercase font-bold mt-4 text-xl'>
+					<h2 className='col-span-full justify-self-center mt-4 uppercase font-bold text-xl sm:row-start-1 sm:col-start-2 sm:justify-self-start'>
 						{data.product.name}
 					</h2>
-					<p className='justify-self-center col-span-full sm:row-start-2  sm:col-start-2  sm:justify-self-start font-bold text-lg text-red-500 '>
+					<p className='justify-self-center col-span-full font-bold text-lg text-red-500 sm:row-start-2  sm:col-start-2  sm:justify-self-start '>
 						{Intl.NumberFormat('pl-PL', {
 							style: 'currency',
 							currency: 'PLN',
 						}).format(data.product.price)}
 					</p>
-					<div className=' col-span-full sm:row-start-3 sm:col-start-2 sm:justify-self-start justify-self-center '>
-						<Stars rating={avarageReviewsScore}/>
+					<div className=' col-span-full justify-self-center sm:row-start-3 sm:col-start-2 sm:justify-self-start  '>
+						<Stars rating={avarageReviewsScore} />
 					</div>
-					<p className='px-4 xs:px-6 sm:px-0 col-span-full text-center sm:row-start-4 sm:col-start-2 sm:text-left sm:max-w-[30rem]'>
+					<p className='px-4 col-span-full text-center xs:px-6 sm:px-0  sm:row-start-4 sm:col-start-2 sm:text-left sm:max-w-[30rem]'>
 						{data.product.description}
 					</p>
-					<div className='flex flex-col items-center mt-4 mb-4 gap-3 xs:flex-row justify-center col-span-full sm:col-start-2 sm:justify-start sm:items-stretch '>
+					<div className='flex flex-col items-center justify-center col-span-full mt-4 mb-4 gap-3 xs:flex-row sm:col-start-2 sm:justify-start sm:items-stretch '>
 						<div className='flex'>
 							<span
-								className='border flex items-center p-2 cursor-pointer '
+								className='flex items-center p-2 border cursor-pointer '
 								onClick={() => {
 									if (amountOfProducts > 1)
 										setAmountOfProducts(amountOfProducts - 1);
 								}}>
 								<IconMinus />
 							</span>
-							<p className=' w-10 text-center border-y flex justify-center items-center  '>
+							<p className=' flex justify-center items-center w-10 text-center border-y  '>
 								{amountOfProducts}
 							</p>
 							<span className='flex items-center p-2 border cursor-pointer'>
@@ -69,13 +67,15 @@ export default function ProductPage() {
 							</span>
 						</div>
 						<button
-							className='bg-black text-white uppercase p-2'
+							className='p-2 bg-black text-white uppercase'
 							onClick={() => {
 								if (data.product) {
 									const product = {
 										name: data.product?.name,
 										id: data.product?.id,
 										amount: amountOfProducts,
+										price: data.product.price,
+										img: data.product.images[0].url,
 									};
 									addToCart(product);
 								}
