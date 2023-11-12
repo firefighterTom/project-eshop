82d95674b2d8e9b526b5b34c7f77502ac25bc34e
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-12 18:19:03 +0100

    The source of import Layout has been changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,27 @@
 import { client } from 'apollo/client';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from "next-auth/react"
-import '../styles/global.css'
-import { Layout } from 'components/Layout';
+import { SessionProvider } from 'next-auth/react';
+import '../styles/global.css';
+import { Layout } from 'components/Layout/Layout';
 import { ShowingComponentProvider } from 'context/showingComponent';
 import { CartProvider } from 'context/cart';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}: AppProps) {
 	return (
 		<SessionProvider session={session}>
-		<ApolloProvider client={client}>
-		<ShowingComponentProvider>
-				<CartProvider>
-			<Layout>
-			<Component {...pageProps} />
-			</Layout>
-			</CartProvider>
-			</ShowingComponentProvider>
-		</ApolloProvider>
+			<ApolloProvider client={client}>
+				<ShowingComponentProvider>
+					<CartProvider>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</CartProvider>
+				</ShowingComponentProvider>
+			</ApolloProvider>
 		</SessionProvider>
 	);
 }
