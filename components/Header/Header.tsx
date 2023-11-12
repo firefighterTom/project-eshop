bcb4352ee7facba1e82eb3db123b39f31ff8d55b
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-12 17:35:51 +0100

    changed the font of h1 and added value to href

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,16 +7,16 @@ export function Header() {
 		<div className='relative z-0 mb-10'>
 			<div>
 				<div className='absolute w-full h-full z-20 flex flex-col justify-center items-center text-white '>
-					<h1 className='uppercase font-gabarito text-2xl sm:text-3xl lg:text-4xl mb-5'>
+					<h1 className='mb-5 uppercase font-merriweather text-2xl text-center sm:text-3xl lg:text-4xl  '>
 						Sports equipment
 					</h1>
-					<p className='text-sm sm:text-base lg:text-lg text-center mx-2 mb-2'>
+					<p className=' mx-2 mb-2 text-center text-sm sm:text-base lg:text-lg  '>
 						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis,
 						explicabo!
 					</p>
 					<Link
-						href={'/'}
-						className='py-2 px-3 mt-2 bg-[#70a9a1] text-xs sm:text-sm lg:text-base rounded hover:bg-[#70a9a1]/[0.9]'>
+						href={'/allProducts'}
+						className='py-2 px-3 mt-2 rounded bg-button-color text-xs sm:text-sm lg:text-base hover:bg-button-color/[0.9]'>
 						View collection
 					</Link>
 				</div>
