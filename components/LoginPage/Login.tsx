3a7558bf7e09828cbb4e6702d7ebe6dc76ffc756
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-12 17:49:14 +0100

    added context to switch with Registration component

--- components/LoginPage/Login.tsx
+++ components/LoginPage/Login.tsx
@@ -4,15 +4,10 @@ import * as yup from 'yup';
 import { loginSchema } from '../../schema/schemaRegister';
 import { Input } from './Input';
 import { signIn } from 'next-auth/react';
-// I will changed the type any and props if the context idea turn out to be good
+import { useShowingComponentContext } from 'context/showingComponent';
 
-
-type PropsType={
-	isOpen:boolean,
-	setIsOpen:(isOpen:boolean)=>void
-
-}
-export function Login(props:PropsType) {
+export function Login() {
+	const context = useShowingComponentContext();
 	type FormData = yup.InferType<typeof loginSchema>;
 	const {
 		register,
@@ -23,7 +18,7 @@ export function Login(props:PropsType) {
 	});
 
 	return (
-		<div className='w-[20%] min-w-[15rem]  bg-white py-4   mb-3 px-4'>
+		<div className='w-[20%] min-w-[15rem]  bg-white py-4 mb-3 px-4'>
 			<h2 className='text-center mb-4 font-bold uppercase'>Log in</h2>
 			<form
 				className='flex flex-col justify-center '
@@ -45,7 +40,18 @@ export function Login(props:PropsType) {
 				<button className='bg-black text-white text-sm py-1 font-bold uppercase  mt-4 mb-2 '>
 					Login
 				</button>
-				<p className='text-xs text-center text-blue-500'>{`Don't have an account?`} <span className='cursor-pointer' onClick={()=>props.setIsOpen(!props.isOpen)}>Sign up</span></p>
+				<p className='text-xs text-center'>
+					{`Don't have an account?`}
+					<span
+						className=' text-blue-500 cursor-pointer'
+						onClick={() =>
+							context.openComponent(
+								'switchBetweenLoginAndRegistrationComponents'
+							)
+						}>
+						Sign up
+					</span>
+				</p>
 			</form>
 		</div>
 	);
--- pages/Account.tsx
+++ pages/Account.tsx
@@ -1,15 +1,16 @@
 import { Login } from 'components/LoginPage/Login';
 import { Registration } from 'components/LoginPage/RegistrationPage';
+import { useShowingComponentContext } from 'context/showingComponent';
 import Image from 'next/image'
-import {  useState } from 'react';
 
 export default function LoginPage() {
-	const [isOpen,setIsOpen]=useState(false)
+	const context =useShowingComponentContext();
+
 	return (
 		<div className=' h-full  relative flex flex-col justify-center items-center '>
 			<Image src='/tennisBallAndShoes.jpg' fill  className=' -z-10 object-cover ' alt='Ball in the corner'/>
-			{isOpen && <Login  isOpen={isOpen} setIsOpen={setIsOpen}/>}
-			{!isOpen &&<Registration isOpen={isOpen} setIsOpen={setIsOpen}/>}
+			{!context.switchBetweenLoginAndRegistrationComponents.isOpen && <Login />}
+			{context.switchBetweenLoginAndRegistrationComponents.isOpen &&<Registration />}
 			</div>
 	
 	);
