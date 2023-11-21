45a819a1ef2ae8bdadef485ca46f1d6e8f393aed
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-21 21:09:16 +0100

    added functionality to show component or redirect depending on status and session

--- pages/Account.tsx
+++ pages/Account.tsx
@@ -1,17 +1,35 @@
 import { Login } from 'components/LoginPage/Login';
 import { Registration } from 'components/LoginPage/RegistrationPage';
 import { useShowingComponentContext } from 'context/showingComponent';
-import Image from 'next/image'
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import router from 'next/router';
 
 export default function LoginPage() {
-	const context =useShowingComponentContext();
+	const context = useShowingComponentContext();
+	const { data: session, status } = useSession();
 
-	return (
-		<div className=' h-full  relative flex flex-col justify-center items-center '>
-			<Image src='/tennisBallAndShoes.jpg' fill  className=' -z-10 object-cover ' alt='Ball in the corner'/>
-			{!context.switchBetweenLoginAndRegistrationComponents.isOpen && <Login />}
-			{context.switchBetweenLoginAndRegistrationComponents.isOpen &&<Registration />}
+	if (status === 'loading') {
+		return <div>Loading...</div>;
+	}
+	if (session) {
+		router.replace('/');
+	}
+	if (!session)
+		return (
+			<div className=' h-full  relative flex flex-col justify-center items-center '>
+				<Image
+					src='/tennisBallAndShoes.jpg'
+					fill
+					className=' -z-10 object-cover '
+					alt='Ball in the corner'
+				/>
+				{!context.switchBetweenLoginAndRegistrationComponents.isOpen && (
+					<Login />
+				)}
+				{context.switchBetweenLoginAndRegistrationComponents.isOpen && (
+					<Registration />
+				)}
 			</div>
-	
-	);
+		);
 }
