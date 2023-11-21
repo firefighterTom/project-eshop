5cee7b30f6a31b9b8364816f7be38043661be451
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-21 20:43:02 +0100

    added ConfirmToRemoveItemFromCart

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -5,20 +5,24 @@ import { PanelMenu } from './PanelMenu';
 import { useShowingComponentContext } from 'context/showingComponent';
 import IconOpenMenuBar from '../../assets/icon-open.svg';
 import { Notification } from 'components/Notification/Notification';
+import { ConfirmToRemoveItemFromCart } from 'components/Notification/ConfirmToRemoveItemFromCart';
 
 export function Nav() {
 	const context = useShowingComponentContext();
+
+	
 	return (
 		<div className='w-full bg-white'>
 			{context.addedToCartNotificationComponent.isOpen && <Notification />}
 			{context.panelMenuComponent.isOpen && <PanelMenu />}
+			{context.confirmToDeleteItemFromCartNotification.isOpen && <ConfirmToRemoveItemFromCart/>}
 			<div className='relative flex  px-5 py-5 mx-auto  max-w-screen-2xl bg-white text-black  '>
 				<button
 					className='basis-[30%]'
 					onClick={() => context.visibilityToggle('panelMenuComponent')}>
 					<IconOpenMenuBar className='w-6 h-6 xs:w-[30px] xs:h-[30px] md:w-[35px] md:h-[35px]' />
 				</button>
-				<div className=' basis-[40%] text-center text-2xl sm:text-3xl font-merriweather uppercase lg:w-[19rem] '>
+				<div className=' basis-[40%] text-center text-xl sm:text-3xl font-merriweather uppercase lg:w-[19rem] '>
 					<Link href={'/'}>E-shop</Link>
 				</div>
 				<div className='flex items-center justify-end basis-[30%] gap-4 sm:gap-6'>
