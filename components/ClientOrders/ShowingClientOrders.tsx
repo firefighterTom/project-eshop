28201a127d06af91c10ea1c39db8ccc45e9cbacd
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-21 21:19:16 +0100

    added the view if client doesn't have orders

--- components/ClientOrders/ShowingClientOrders.tsx
+++ components/ClientOrders/ShowingClientOrders.tsx
@@ -1,18 +1,41 @@
-import { useGetClientOrdersQuery } from "generated/graphql";
+import { useGetClientOrdersQuery } from 'generated/graphql';
+import { AccordionOrders } from 'components/Accordion/AccordionOrders';
 
 type ShowingClientOrdersProps = {
 	clientEmail: string;
 };
 
 export function ShowingClientOrders(props: ShowingClientOrdersProps) {
-    
-    const data =useGetClientOrdersQuery({variables:{
-        email:props.clientEmail
-    }})
-    console.log(data)
+	const data = useGetClientOrdersQuery({
+		variables: {
+			email: props.clientEmail,
+		},
+	});
+	const numberOfOrders = data.data?.orders.length;
+	if (!numberOfOrders)
+		return (
+			<div className='flex flex-col h-full justify-center items-center text-center'>
+				<p className='uppercase font-bold'>
+					You haven&apos;t ordered anything yet !
+				</p>
+			</div>
+		);
 
-
-
-	return <>
-    </>;
+	return (
+		<>
+			{data.data &&
+				data.data.orders.map((order) => {
+					const id = order.id;
+					const total = order.total;
+					return (
+						<div key={id} className='flex flex-col items-center'>
+							<AccordionOrders
+								orderItems={order.orderItems}
+								id={id}
+								total={total}></AccordionOrders>
+						</div>
+					);
+				})}
+		</>
+	);
 }
