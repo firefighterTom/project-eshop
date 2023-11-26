5e0973bd8868eef92f8c1258c31b609ff4f38806
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-11-26 22:07:56 +0100

    removed file from context folder

--- context/schemaProductCart.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-export const schemaProductCart = yup.object({
-	id: yup.string().required(),
-	name: yup.string().required(),
-	price: yup.number().required(),
-	amount: yup.number().max(20).required(),
-	img: yup.string().required(),
-});
-export type SchemaProductCart = yup.InferType<typeof schemaProductCart>;
