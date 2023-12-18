dc5fe9a6cd5c5b45195e964524a9d11d5d545da9
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2023-12-18 21:52:35 +0100

    Corrected typo average. Wrapped proudct into link

--- components/Search/Components/Hits.tsx
+++ components/Search/Components/Hits.tsx
@@ -3,45 +3,51 @@ import { connectStateResults } from 'react-instantsearch-dom';
 import Image from 'next/image';
 import { Stars } from 'components/Stars/Stars';
 import { averageReviewScore } from 'utilities/avarageReviewScore';
+import Link from 'next/link';
+import { useShowingComponentContext } from 'context/showingComponent';
 
 export function Hits({
 	allSearchResults,
 	searchState,
 }: StateResultsProvided<{
 	name: string;
+	slug:string;
 	description: string;
 	price: number;
 	images: { url: string }[];
 	reviews: { rating: number }[];
 }>) {
 	if (!searchState?.query) return <></>;
+	const context = useShowingComponentContext();
 
 	return (
 		<ul>
 			{allSearchResults.hits.map((product) => {
-				const avarageReviewsScore =
+				const averageReviewsScore =
 					product.reviews.length >= 0 ? averageReviewScore(product.reviews) : 0;
 				return (
 					<li key={product.objectID} className='py-3 border-b'>
-						<article className='grid grid-rows-2 grid-cols-[1fr_2fr_1fr] text-sm'>
-							<Image
-								width={30}
-								height={30}
-								src={product.images[0].url}
-								alt={product.name}
-								className=' sm:w-[50px] sm:h-[50px] row-span-full justify-self-center'
-							/>
-							<h3 className=''>{product.name}</h3>
-							<p className=' row-span-full col-start-3 self-center'>
-								{Intl.NumberFormat('pl-PL', {
-									style: 'currency',
-									currency: 'PLN',
-								}).format(product.price)}
-							</p>
-							<div>
-								<Stars rating={avarageReviewsScore} />
-							</div>
-						</article>
+						<Link href={`/products/${product.slug}`} onClick={() => context.visibilityToggle('searchComponent')}>
+							<article className='grid grid-rows-2 grid-cols-[1fr_2fr_1fr] text-sm'>
+								<Image
+									width={30}
+									height={30}
+									src={product.images[0].url}
+									alt={product.name}
+									className=' sm:w-[50px] sm:h-[50px] row-span-full justify-self-center'
+								/>
+								<h3>{product.name}</h3>
+								<p className=' row-span-full col-start-3 self-center'>
+									{Intl.NumberFormat('pl-PL', {
+										style: 'currency',
+										currency: 'PLN',
+									}).format(product.price)}
+								</p>
+								<div>
+									<Stars rating={averageReviewsScore} />
+								</div>
+							</article>
+						</Link>
 					</li>
 				);
 			})}
