382c1dcf071e69f1761757c21e13560d40247526
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2024-01-08 21:49:11 +0100

    added ButtonPanelMenu and removed element which are in this component

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
 import IconShoppingCart from '../../assets/icon-shoppingCart.svg';
 import IconSearch from '../../assets/icon-search.svg';
-import { PanelMenu } from './PanelMenu';
 import { useShowingComponentContext } from 'context/showingComponent';
-import IconOpenMenuBar from '../../assets/icon-open.svg';
 import { Notification } from 'components/Notification/Notification';
 import { ConfirmToRemoveItemFromCart } from 'components/Notification/ConfirmToRemoveItemFromCart';
 import { useCartContext } from 'context/cart';
+import { ButtonPanelMenu } from './ButtonPanelMenu';
 
 export function Nav() {
 	const context = useShowingComponentContext();
@@ -16,18 +15,13 @@ export function Nav() {
 	return (
 		<div className='w-full bg-white'>
 			{context.addedToCartNotificationComponent.isOpen && <Notification />}
-			{context.panelMenuComponent.isOpen && <PanelMenu />}
+			
 			{context.confirmToDeleteItemFromCartNotification.isOpen && (
 				<ConfirmToRemoveItemFromCart />
 			)}
 			<div className='relative flex  px-5 py-5 mx-auto  max-w-screen-2xl bg-white text-black  '>
-				<div className='basis-[30%]'>
-					<button
-						className=''
-						onClick={() => context.visibilityToggle('panelMenuComponent')}>
-						<IconOpenMenuBar className='w-6 h-6 xs:w-[30px] xs:h-[30px] md:w-[35px] md:h-[35px]' />
-					</button>
-				</div>
+				<ButtonPanelMenu/>
+				
 				<div className=' basis-[40%] text-center text-xl sm:text-3xl font-merriweather uppercase lg:w-[19rem] '>
 					<Link href={'/'}>E-shop</Link>
 				</div>
