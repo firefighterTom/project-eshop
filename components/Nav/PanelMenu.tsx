d356008792af677a707f9465f3b62c081927c5f9
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2024-01-08 22:00:11 +0100

    removed context showing Notification and used instead closePanelMenu function

--- components/Nav/PanelMenu.tsx
+++ components/Nav/PanelMenu.tsx
@@ -3,17 +3,18 @@ import Link from 'next/link';
 import IconCloseMenuBar from '../../assets/icon-close.svg';
 import { signOut, useSession } from 'next-auth/react';
 
+type PanelMenuProps = {
+	closePanelMenu: () => void;
+};
 const linkNav = [
 	{ link: '/myOrders', linkName: 'My orders' },
 	{ link: '/allProducts', linkName: 'Products' },
 	{ link: '/cart', linkName: 'Cart' },
 ];
-
-export function PanelMenu() {
+export function PanelMenu({ closePanelMenu }: PanelMenuProps) {
+	console.log(closePanelMenu);
 	const { data: session } = useSession();
 	const firstLetterOfTheName = session?.user?.name?.charAt(0).toUpperCase();
-
-	const context = useShowingComponentContext();
 
 	return (
 		<>
@@ -21,13 +22,13 @@ export function PanelMenu() {
 			<div className='absolute bg-quartz-gray text-white left-0 top-0 h-screen z-40 flex w-1/3 min-w-[13rem] max-w-[15rem]  '>
 				<div className='relative flex flex-col mt-8 w-full'>
 					<Link
-						onClick={() => context.visibilityToggle('panelMenuComponent')}
+						onClick={() => closePanelMenu()}
 						href={'/'}
 						className='pl-2 font-bold uppercase text-2xl'>
 						E-shop
 					</Link>
 					<button
-						onClick={() => context.visibilityToggle('panelMenuComponent')}
+						onClick={() => closePanelMenu()}
 						className='absolute right-3 top-[1px]'>
 						<IconCloseMenuBar />
 					</button>
@@ -44,7 +45,7 @@ export function PanelMenu() {
 							return (
 								<Link
 									key={el.linkName}
-									onClick={() => context.visibilityToggle('panelMenuComponent')}
+									onClick={() => closePanelMenu()}
 									href={`${el.link}`}
 									className='block py-3 hover:bg-white hover:text-quartz-gray duration-200  pl-2 border-b-[1px]  '>
 									{el.linkName}
@@ -62,7 +63,7 @@ export function PanelMenu() {
 					{!session && (
 						<Link
 							href={'/login'}
-							onClick={() => context.visibilityToggle('panelMenuComponent')}
+							onClick={() => closePanelMenu()}
 							className='mx-auto py-2 px-3 mt-12 bg-button-color text-xs sm:text-sm lg:text-base rounded hover:bg-button-color/[0.9] '>
 							Log In
 						</Link>
