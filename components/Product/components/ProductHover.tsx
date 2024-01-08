d84bac9eb04f946b2c8c0fde764e36d2c66be3ab
firefighterTom <101346243+firefighterTom@users.noreply.github.com>
2024-01-08 21:55:47 +0100

    added function of notification appearing after clicking the button

--- components/Product/components/ProductHover.tsx
+++ components/Product/components/ProductHover.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Eye from '../../../assets/eye.svg';
 import { useCartContext } from 'context/cart';
+import { useShowingComponentContext } from 'context/showingComponent';
 
 type productHoverProps = {
 	product: {
@@ -14,6 +15,7 @@ type productHoverProps = {
 
 export function ProductHover(props: productHoverProps) {
 	const { addToCart } = useCartContext();
+	const context = useShowingComponentContext();
 	const product = {
 		name: props.product.name,
 		id: props.product.id,
@@ -25,13 +27,18 @@ export function ProductHover(props: productHoverProps) {
 		<div className='absolute flex flex-col justify-center items-center w-[130%] h-full bg-quartz-gray/90 gap-3 animate-opacity '>
 			<button
 				className='inline-block text-black text-center bg-white p-1 px-3 rounded-sm border-white hover:bg-white/[0.7] '
-				onClick={() => addToCart(product)}>
+				onClick={() => {
+					addToCart(product);
+					context.visibilityToggle('addedToCartNotificationComponent');
+				}}>
 				Add to cart
 			</button>
-			
-				<Link href={`products${props.product.linkToProduct}`} className='text-white'>
-					<Eye className='bg-white fill-quartz-gray rounded-sm border-white hover:bg-white/[0.7] ' />
-				</Link>
+
+			<Link
+				href={`products${props.product.linkToProduct}`}
+				className='text-white'>
+				<Eye className='bg-white fill-quartz-gray rounded-sm border-white hover:bg-white/[0.7] ' />
+			</Link>
 		</div>
 	);
 }
